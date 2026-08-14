151088169e0efe05368b516de76c18f5422cf813
enyopereira <enyo.pereira@ufes.br>
2026-08-14 04:15:03 -0300

    Refactor button initialization and event handling

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const botaoIniciar = document.getElementById("botaoIniciar");
+document.addEventListener("DOMContentLoaded", () => {
+  const botaoIniciar = document.getElementById("botaoIniciar");
 
-// Mostra o botão depois de 7 segundos
-setTimeout(() => {
-  botaoIniciar.style.display = "block";
-  botaoIniciar.focus();
-}, 7000);
+  setTimeout(() => {
+    botaoIniciar.style.display = "block";
+    botaoIniciar.focus();
+  }, 7000);
 
-// Ação realizada ao selecionar o botão
-botaoIniciar.addEventListener("click", () => {
-  botaoIniciar.style.display = "none";
-
-  console.log("Aplicação iniciada");
+  botaoIniciar.addEventListener("click", () => {
+    botaoIniciar.style.display = "none";
+  });
 });
